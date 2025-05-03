1850d1e8af767fb2f65bb779fc9c728b3be88533
pukkeh <gogimissing98@gmail.com>
2025-05-03 13:00:08 +0300

    Update autoplay.lua

--- autoplay.lua
+++ autoplay.lua
@@ -1,3 +1,48 @@
+function notify(text,text2,length)
+	task.spawn(function()
+		local LnotifyCount = notifyCount+1
+		local notificationPinned = false
+		notifyCount = notifyCount+1
+		if pinNotification then pinNotification:Disconnect() end
+		pinNotification = PinButton.MouseButton1Click:Connect(function()
+			task.spawn(function()
+				pinNotification:Disconnect()
+				notificationPinned = true
+				Title_2.BackgroundTransparency = 1
+				wait(0.5)
+				Title_2.BackgroundTransparency = 0
+			end)
+		end)
+		Notification:TweenPosition(UDim2.new(1, Notification.Position.X.Offset, 1, 0), "InOut", "Quart", 0.5, true, nil)
+		wait(0.6)
+		local closepressed = false
+		if text2 then
+			Title_2.Text = text
+			Text_2.Text = text2
+		else
+			Title_2.Text = 'Notification'
+			Text_2.Text = text
+		end
+		Notification:TweenPosition(UDim2.new(1, Notification.Position.X.Offset, 1, -100), "InOut", "Quart", 0.5, true, nil)
+		CloseButton.MouseButton1Click:Connect(function()
+			Notification:TweenPosition(UDim2.new(1, Notification.Position.X.Offset, 1, 0), "InOut", "Quart", 0.5, true, nil)
+			closepressed = true
+			pinNotification:Disconnect()
+		end)
+		if length and isNumber(length) then
+			wait(length)
+		else
+			wait(10)
+		end
+		if LnotifyCount == notifyCount then
+			if closepressed == false and notificationPinned == false then
+				pinNotification:Disconnect()
+				Notification:TweenPosition(UDim2.new(1, Notification.Position.X.Offset, 1, 0), "InOut", "Quart", 0.5, true, nil)
+			end
+			notifyCount = 0
+		end
+	end)
+end
 
 if not fireproximityprompt then
 
